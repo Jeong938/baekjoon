d696ec15ed50d3e39646b8f20a1fe3e307ebb64c
Jeong938 <165477960+Jeong938@users.noreply.github.com>
2025-05-02 11:25:28 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17756 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		long s1=sc.nextLong();
+		long s2=sc.nextLong();
+		long s3=sc.nextLong();
+		System.out.println(s1+s2+s3);
+		
+	}
+
+}

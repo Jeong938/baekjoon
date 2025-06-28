46f944815676104bf825ea7decd3e4ada48524c7
Jeong938 <165477960+Jeong938@users.noreply.github.com>
2025-06-28 20:54:21 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 212 ms, Memory: 6612 KB -BaekjoonHub

---
+++ C#/백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cs
@@ -0,0 +1,24 @@
+using System;
+namespace ReadLine
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <=N; i++)
+            {
+                for (int j= 0; j < i; j++)
+                {
+                    Console.Write("*");
+                
+                }
+                Console.WriteLine();
+            }
+
+
+
+        }
+    }
+}
